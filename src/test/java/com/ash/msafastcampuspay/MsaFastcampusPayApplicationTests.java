1d03f742816cfa5af0078becc9bd107bd6ae500e
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2024-12-30 16:57:37 +0900

    init project

---
+++ src/test/java/com/ash/msafastcampuspay/MsaFastcampusPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ash.msafastcampuspay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaFastcampusPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
