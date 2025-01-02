fa3ab7a3d73fba6b2c5744fe87bd70882419ddb9
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2025-01-02 10:45:25 +0900

    Git 누락 파일 추가.

---
+++ membership-service/src/test/java/com/ash/membership/MainTests.java
@@ -0,0 +1,13 @@
+package com.ash.membership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
